ea7f7ea48a95215f740ddf1b05b5b96aeb190cd9
flashcharge99 <139184786+flashcharge99@users.noreply.github.com>
2024-04-25 23:25:49 +0700

    Add files via upload

--- allxray/trialxray.sh
+++ allxray/trialxray.sh
@@ -49,55 +49,80 @@ vmlink2=`cat<<EOF
 "tls": "none"
 }
 EOF`
+vmlink3=`cat<<EOF
+{
+"v": "2",
+"ps": "${user}",
+"add": "${domain}",
+"port": "443",
+"id": "${uuid}",
+"aid": "0",
+"net": "httpupgrade",
+"path": "/vmess-hup",
+"type": "none",
+"host": "$domain",
+"tls": "tls"
+}
+EOF`
 vmesslink1="vmess://$(echo $vmlink1 | base64 -w 0)"
 vmesslink2="vmess://$(echo $vmlink2 | base64 -w 0)"
+vmesslink3="vmess://$(echo $vmlink3 | base64 -w 0)"
 
 vlesslink1="vless://$uuid@$domain:443?path=/vless&security=tls&encryption=none&host=$domain&type=ws&sni=$domain#$user"
 vlesslink2="vless://$uuid@$domain:80?path=/vless&security=none&encryption=none&host=$domain&type=ws#$user"
 vlesslink3="vless://$uuid@$domain:443?security=tls&encryption=none&headerType=none&type=tcp&sni=$domain&flow=xtls-rprx-vision&fp=chrome#$user"
+vlesslink4="vless://$uuid@$domain:443?path=/vless-hup&security=tls&encryption=none&host=$domain&type=httpupgrade&sni=$domain#$user"
 
 trojanlink1="trojan://$pwtr@$domain:443?path=/trojan&security=tls&host=$domain&type=ws&sni=$domain#$user"
 trojanlink2="trojan://$pwtr@$domain:80?path=/trojan&security=none&host=$domain&type=ws#$user"
 trojanlink3="trojan://$pwtr@$domain:443?security=tls&type=tcp&sni=$domain#$user"
+trojanlink4="trojan://$pwtr@$domain:443?path=/trojan-hup&security=tls&host=$domain&type=httpupgrade&sni=$domain#$user"
 
 cat > /var/www/html/allxray/allxray-$user.txt << END
 ========================================
         ----- [ All Xray ] -----
 ========================================
-Domain      : $domain
-ISP         : $ISP
-City        : $CITY
-Port TLS    : 443
-Port NTLS   : 80
-Network     : TCP, Websocket
-Alpn        : h2, http/1.1
-Expired On  : $exp
+Domain            : $domain
+ISP               : $ISP
+City              : $CITY
+Port WS TLS       : 443
+Port WS nTLS      : 80
+Port HTTPupgrade  : 443
+Network           : TCP, Websocket, HTTPupgrade
+Alpn              : h2, http/1.1
+Expired On        : $exp
 ========================================
         ----- [ Vmess Link ] -----
 ========================================
-Link TLS   : $vmesslink1
+Link WS TLS       : $vmesslink1
 ========================================
-Link NTLS  : $vmesslink2
+Link WS nTLS      : $vmesslink2
+========================================
+Link HTTPupgrade  : $vmesslink3
 ========================================
 
 ========================================
         ----- [ Vless Link ] -----
 ========================================
-Link TLS   : $vlesslink1
+Link WS TLS       : $vlesslink1
 ========================================
-Link NTLS  : $vlesslink2
+Link WS nTLS      : $vlesslink2
 ========================================
-Link XTLS  : $vlesslink3
+Link XTLS         : $vlesslink3
+========================================
+Link HTTPupgrade  : $vlesslink4
 ========================================
 
 ========================================
        ----- [ Trojan Link ] -----
 ========================================
-Link TLS   : $trojanlink1
+Link WS TLS       : $trojanlink1
 ========================================
-Link NTLS  : $trojanlink2
+Link WS nTLS      : $trojanlink2
 ========================================
-Link TCP   : $trojanlink3
+Link TCP TLS      : $trojanlink3
+========================================
+Link HTTPupgrade  : $trojanlink4
 ========================================
 END
 systemctl restart xray
@@ -105,43 +130,50 @@ clear
 echo -e "————————————————————————————————————————————————————" | tee -a /user/log-allxray-$user.txt
 echo -e "              ----- [ All Xray ] -----              " | tee -a /user/log-allxray-$user.txt
 echo -e "————————————————————————————————————————————————————" | tee -a /user/log-allxray-$user.txt
-echo -e "Domain       : $domain" | tee -a /user/log-allxray-$user.txt
-echo -e "ISP          : $ISP" | tee -a /user/log-allxray-$user.txt
-echo -e "City         : $CITY" | tee -a /user/log-allxray-$user.txt
-echo -e "Port TLS     : 443" | tee -a /user/log-allxray-$user.txt
-echo -e "Port NTLS    : 80" | tee -a /user/log-allxray-$user.txt
-echo -e "Network      : TCP, Websocket" | tee -a /user/log-allxray-$user.txt
-echo -e "Alpn         : h2, http/1.1" | tee -a /user/log-allxray-$user.txt
-echo -e "Expired On   : $exp" | tee -a /user/log-allxray-$user.txt
-echo -e "Link Akun    : https://$domain/allxray/allxray-$user.txt" | tee -a /user/log-allxray-$user.txt
+echo -e "Domain             : $domain" | tee -a /user/log-allxray-$user.txt
+echo -e "ISP                : $ISP" | tee -a /user/log-allxray-$user.txt
+echo -e "City               : $CITY" | tee -a /user/log-allxray-$user.txt
+echo -e "Port WS TLS        : 443" | tee -a /user/log-allxray-$user.txt
+echo -e "Port WS nTLS       : 80" | tee -a /user/log-allxray-$user.txt
+echo -e "Port HTTPupgrade   : 443" | tee -a /user/log-allxray-$user.txt
+echo -e "Network            : TCP, Websocket, HTTPupgrade" | tee -a /user/log-allxray-$user.txt
+echo -e "Alpn               : h2, http/1.1" | tee -a /user/log-allxray-$user.txt
+echo -e "Expired On         : $exp" | tee -a /user/log-allxray-$user.txt
+echo -e "Link Akun          : https://$domain/allxray/allxray-$user.txt" | tee -a /user/log-allxray-$user.txt
 echo -e "————————————————————————————————————————————————————" | tee -a /user/log-allxray-$user.txt
 echo -e "             ----- [ Vmess Link ] -----             " | tee -a /user/log-allxray-$user.txt
 echo -e "————————————————————————————————————————————————————" | tee -a /user/log-allxray-$user.txt
-echo -e "Link TLS   : $vmesslink1" | tee -a /user/log-allxray-$user.txt
+echo -e "Link WS TLS       : $vmesslink1" | tee -a /user/log-allxray-$user.txt
 echo -e "————————————————————————————————————————————————————" | tee -a /user/log-allxray-$user.txt
-echo -e "Link NTLS  : $vmesslink2" | tee -a /user/log-allxray-$user.txt
+echo -e "Link WS nTLS      : $vmesslink2" | tee -a /user/log-allxray-$user.txt
+echo -e "————————————————————————————————————————————————————" | tee -a /user/log-allxray-$user.txt
+echo -e "Link HTTPupgrade  : $vmesslink3" | tee -a /user/log-allxray-$user.txt
 echo -e "————————————————————————————————————————————————————" | tee -a /user/log-allxray-$user.txt
 echo -e " " | tee -a /user/log-allxray-$user.txt
 echo -e " " | tee -a /user/log-allxray-$user.txt
 echo -e "————————————————————————————————————————————————————" | tee -a /user/log-allxray-$user.txt
 echo -e "             ----- [ Vless Link ] -----             " | tee -a /user/log-allxray-$user.txt
 echo -e "————————————————————————————————————————————————————" | tee -a /user/log-allxray-$user.txt
-echo -e "Link TLS   : $vlesslink1" | tee -a /user/log-allxray-$user.txt
+echo -e "Link WS TLS       : $vlesslink1" | tee -a /user/log-allxray-$user.txt
 echo -e "————————————————————————————————————————————————————" | tee -a /user/log-allxray-$user.txt
-echo -e "Link NTLS  : $vlesslink2" | tee -a /user/log-allxray-$user.txt
+echo -e "Link WS nTLS      : $vlesslink2" | tee -a /user/log-allxray-$user.txt
 echo -e "————————————————————————————————————————————————————" | tee -a /user/log-allxray-$user.txt
-echo -e "Link XTLS  : $vlesslink3" | tee -a /user/log-allxray-$user.txt
+echo -e "Link XTLS         : $vlesslink3" | tee -a /user/log-allxray-$user.txt
+echo -e "————————————————————————————————————————————————————" | tee -a /user/log-allxray-$user.txt
+echo -e "Link HTTPupgrade  : $vlesslink4" | tee -a /user/log-allxray-$user.txt
 echo -e "————————————————————————————————————————————————————" | tee -a /user/log-allxray-$user.txt
 echo -e " " | tee -a /user/log-allxray-$user.txt
 echo -e " " | tee -a /user/log-allxray-$user.txt
 echo -e "————————————————————————————————————————————————————" | tee -a /user/log-allxray-$user.txt
 echo -e "            ----- [ Trojan Link ] -----             " | tee -a /user/log-allxray-$user.txt
 echo -e "————————————————————————————————————————————————————" | tee -a /user/log-allxray-$user.txt
-echo -e "Link TLS   : $trojanlink1" | tee -a /user/log-allxray-$user.txt
+echo -e "Link WS TLS      : $trojanlink1" | tee -a /user/log-allxray-$user.txt
 echo -e "————————————————————————————————————————————————————" | tee -a /user/log-allxray-$user.txt
-echo -e "Link NTLS  : $trojanlink2" | tee -a /user/log-allxray-$user.txt
+echo -e "Link WS nTLS     : $trojanlink2" | tee -a /user/log-allxray-$user.txt
 echo -e "————————————————————————————————————————————————————" | tee -a /user/log-allxray-$user.txt
-echo -e "Link TCP   : $trojanlink3" | tee -a /user/log-allxray-$user.txt
+echo -e "Link TCP TLS     : $trojanlink3" | tee -a /user/log-allxray-$user.txt
+echo -e "————————————————————————————————————————————————————" | tee -a /user/log-allxray-$user.txt
+echo -e "Link HTTPupgrade : $trojanlink4" | tee -a /user/log-allxray-$user.txt
 echo -e "————————————————————————————————————————————————————" | tee -a /user/log-allxray-$user.txt
 echo -e " " | tee -a /user/log-allxray-$user.txt
 echo -e " " | tee -a /user/log-allxray-$user.txt
